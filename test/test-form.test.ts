598128624bdbbbe8ae6ff345e911573eec78aa59
TonyThorne <anthony.thorne@gmail.com>
2022-12-30 18:06:27 +0000

    Feat: Added basic form

--- test/test-form.test.ts
+++ test/test-form.test.ts
@@ -5,12 +5,39 @@ import { describe, expect, it } from 'vitest'
 import TestForm from '../src/pages/test-form.vue'
 
 describe('UI elements', () => {
-  describe('Testing the test form!', () => {
+  describe('Displaying the title', () => {
     it('should render the header Test Form', () => {
       render(TestForm)
       const header = screen.queryByRole('heading', { name: 'Test Form' })
       expect(header).toBeInTheDocument()
       expect(header).toMatchSnapshot()
+    })
+  })
+  describe('Displaying the form', () => {
+    it('has a date & time field', () => {
+      render(TestForm)
+      const date = screen.queryByLabelText('Date / Time')
+      expect(date).toBeInTheDocument()
+    })
+    it('has a name field', () => {
+      render(TestForm)
+      const name = screen.queryByLabelText('Name')
+      expect(name).toBeInTheDocument()
+    })
+    it('has a email field', () => {
+      render(TestForm)
+      const email = screen.queryByLabelText('Email')
+      expect(email).toBeInTheDocument()
+    })
+    it('has a subject field', () => {
+      render(TestForm)
+      const subject = screen.queryByLabelText('Subject')
+      expect(subject).toBeInTheDocument()
+    })
+    it('has a details field', () => {
+      render(TestForm)
+      const details = screen.queryByLabelText('Details')
+      expect(details).toBeInTheDocument()
     })
   })
 })
