fe705c5a5456aaa33609b176388160c4e03be5d0
Tony Thorne <anthony.thorne@gmail.com>
2023-02-22 22:08:50 +0000

    Feat: First version validation

---
+++ src/pages/test.ts
@@ -0,0 +1,17 @@
+{
+    "success": false,
+        "error": {
+        "issues": [
+            {
+                "code": "too_small",
+                "minimum": 2,
+                "type": "string",
+                "inclusive": true,
+                "exact": false,
+                "message": "Must be at least two characters",
+                "path": []
+            }
+        ],
+            "name": "ZodError"
+    }
+}
