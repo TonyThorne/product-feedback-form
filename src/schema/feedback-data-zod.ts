3a6fb28f8a43bd1f67857a8478bd8a6c7d2181ad
TonyThorne <anthony.thorne@gmail.com>
2023-03-04 12:10:43 +0000

    bug: Updated Zod date format

--- src/schema/feedback-data-zod.ts
+++ src/schema/feedback-data-zod.ts
@@ -2,7 +2,7 @@ import * as Z from 'zod'
 
 export const feedbackSchema = Z.object({
   name: Z.string().min(2, 'Must be at least two characters'),
-  dateTime: Z.string(),
+  dateTime: Z.date(),
   email: Z.string().email({ message: 'Invalid email address' }),
   subject: Z.string().min(2),
   details: Z.string().min(3),
